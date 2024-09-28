c687ce7d47ff41524f12d9e7fd399ea4ff69d441
Alan Eicker <alaneicker@gmail.com>
2024-09-28 14:22:00 -0500

    feat: Adds multiply utility function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
 
-module.exports = { add, subtract };
+module.exports = { add, subtract, multiply };
