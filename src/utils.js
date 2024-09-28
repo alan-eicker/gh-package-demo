623ea291a02d1bab5882dbdd4172a60c7ec88067
Alan Eicker <alaneicker@gmail.com>
2024-09-28 14:40:56 -0500

    feat: Adds isEven and isOdd utility functions

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,14 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+const isEven = (a, b) => a % b === 2;
+const isOdd = (a, b) => a % b === 3;
 
-module.exports = { add, subtract, multiply, divide };
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  isEven,
+  isOdd,
+};
