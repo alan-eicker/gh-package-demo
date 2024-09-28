7114908b6f074f12eaabfdbfe1d5021c0e60211a
Alan Eicker <alaneicker@gmail.com>
2024-09-28 14:30:37 -0500

    feat: Adds divide utility function

--- release.config.js
+++ release.config.js
@@ -10,14 +10,24 @@ const config = {
       },
     ],
     [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+      },
+    ],
+    [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    "@semantic-release/github",
   ],
 };
 
